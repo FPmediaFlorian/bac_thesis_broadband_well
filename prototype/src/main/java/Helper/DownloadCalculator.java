4c106e76207754817d16f4addf99ef6ce58d59df
Florian Pichlmann <florian.pichlmann@me.com>
2020-01-31 09:47:08 +0100

    javadoc download calculator

--- prototype/src/main/java/Helper/DownloadCalculator.java
+++ prototype/src/main/java/Helper/DownloadCalculator.java
@@ -6,6 +6,12 @@ public class DownloadCalculator {
     private double stream;
     private SizeSuffix suffix;
 
+    /**
+     * Constructor for a Downloadcalculator
+     * @param size Filesize
+     * @param stream Streamspeed (in MBit/s)
+     * @param suffix Size suffix for size of File
+     */
     public DownloadCalculator(double size, double stream, SizeSuffix suffix) {
         this.size = size;
         this.stream = stream; //Stream in Mbit
@@ -13,9 +19,15 @@ public class DownloadCalculator {
         convertToMBit();
     }
 
+    /**
+     * Default constructor
+     */
     public DownloadCalculator() {
     }
 
+    /**
+     * converts the Filesize from MB,GB or TB to MBit
+     */
     private void convertToMBit() {
         switch (suffix) {
             case TB:
@@ -30,24 +42,45 @@ public class DownloadCalculator {
         }
     }
 
+    /**
+     * calculates downloadtime @ current location in seconds
+     * @return downloadtime @ current location in seconds
+     */
     public double getDownloadtimeSec() {
         return size / stream;
     }
 
+    /**
+     * calculates downloadtime @ BBW in seconds
+     * @return downloadtime @ BBW in seconds
+     */
     public double getBBWdownloadtimeSec() {
         return size / bbwStream;
     }
 
+    /**
+     * sets filesize and size suffix
+     * @param size size of file
+     * @param sizeSuffix size Suffix
+     */
     public void setSize(double size, SizeSuffix sizeSuffix) {
         this.size = size;
         this.suffix = sizeSuffix;
         convertToMBit();
     }
 
+    /**
+     * sets streamspeed
+     * @param stream streamspeed (MBit/s)
+     */
     public void setStream(double stream) {
         this.stream = stream;
     }
 
+    /**
+     * returns filesize in Mbit
+     * @return filesize in Mbit
+     */
     public double getSize() {
         return size;
     }
